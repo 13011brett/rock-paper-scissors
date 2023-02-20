2e0be9d8bf7d785364bb5f00685d7708ffe4a86a
crazy_squirrel <100859780+13011brett@users.noreply.github.com>
2023-02-20 14:15:44 -0500

    Rock Paper Scissors - stepping stones

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
-let t = 5;
-var matches = document.querySelectorAll("p");
-matches.forEach(function func(match){
-    match.innerHTML = "New p Stuff?"; 
-});
+// let t = 5;
+// var matches = document.querySelectorAll("p");
+// matches.forEach(function func(match){
+//     match.innerHTML = "New p Stuff?"; 
+// });
+
+const choices = Object.keys(Object.freeze({
+    rock: 0,
+    paper: 1,
+    scissors: 2
+}))
+var computerChoice = '';
+
+function getComputerChoice(){
+    return computerChoice = choices[getRandomChoice()];
+}
+
+function getRandomChoice(){
+    return Math.floor(Math.random() * (choices.length));
+}
+console.log(getComputerChoice());
+
+const buttons = document.querySelectorAll('button');
+
+
+
 
 
 
